5fc2fa9379f7bc9b9f26d25f47c8d353e810ab2f
Ravi Teja <raviteja281003@gmail.com>
2022-01-24 15:09:40 +0530

    Revert "Update next.config.js"
    This reverts commit a80d5448fc414b72740a0ffb6d2b8636232d3281.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
     domains: ["links.papareact.com"],
   },
   env: {
-    
+    mapbox_key:
+      "pk.eyJ1IjoicjR2aXQzajQiLCJhIjoiY2t5czZ4a3B5MGp3NTJ5czBvcDRmMm5mZiJ9.UOvbPOoWbhagFRpykgX6RQ",
   },
 };
