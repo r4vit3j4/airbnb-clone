a80d5448fc414b72740a0ffb6d2b8636232d3281
Ravi Teja <raviteja281003@gmail.com>
2022-01-24 15:07:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
     domains: ["links.papareact.com"],
   },
   env: {
-    mapbox_key:
-      "pk.eyJ1IjoicjR2aXQzajQiLCJhIjoiY2t5czZ4a3B5MGp3NTJ5czBvcDRmMm5mZiJ9.UOvbPOoWbhagFRpykgX6RQ",
+    
   },
 };
